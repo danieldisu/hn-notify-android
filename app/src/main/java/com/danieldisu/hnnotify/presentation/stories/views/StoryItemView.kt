4ed63082f3773f5fcb6f244cd5f6d741c9ef0c65
danieldisu <danieldiazsuarez@gmail.com>
2020-01-29 20:43:57 +0100

    Changed how the item views are visualized

--- app/src/main/java/com/danieldisu/hnnotify/presentation/stories/views/StoryItemView.kt
+++ app/src/main/java/com/danieldisu/hnnotify/presentation/stories/views/StoryItemView.kt
@@ -1,16 +1,29 @@
 package com.danieldisu.hnnotify.presentation.stories.views
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
 import android.widget.LinearLayout
-import android.widget.TextView
 import com.danieldisu.hnnotify.data.stories.entities.Story
+import com.danieldisu.hnnotify.databinding.ItemViewStoryBinding
 import com.danieldisu.kollectionview.internal.KollectionItemView
+
 
 class StoryItemView(context: Context) : KollectionItemView<Story>, LinearLayout(context) {
 
+  private val binding =
+    ItemViewStoryBinding.inflate(LayoutInflater.from(context), this, true)
+
   override fun bind(viewModel: Story) {
-    val textView = TextView(context)
-    textView.text = viewModel.title
-    addView(textView)
+    binding.titleView.text = viewModel.title
+
+    setOnClickListener {
+      if (viewModel.url != null) {
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(viewModel.url)
+        context.startActivity(i)
+      }
+    }
   }
 }
