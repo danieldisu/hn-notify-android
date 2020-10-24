d88011e71a16d5d4987d0786177957dffdb9c82f
danieldisu <danieldiazsuarez@gmail.com>
2020-10-24 11:05:49 +0200

    Fixed most compose issues

--- app/src/main/java/com/danieldisu/hnnotify/MainActivity.kt
+++ app/src/main/java/com/danieldisu/hnnotify/MainActivity.kt
@@ -2,10 +2,15 @@ package com.danieldisu.hnnotify
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.ui.platform.setContent
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    setContent {
+      Text("Hello World")
+    }
   }
 }
