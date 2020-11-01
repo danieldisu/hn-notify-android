9a1f8543d2be9a576077232f9de68e93b3396f22
danieldisu <danieldiazsuarez@gmail.com>
2020-11-01 19:48:51 +0100

    Extracted bottom bar to method & added FAB

--- app/src/main/java/com/danieldisu/hnnotify/main/MainActivity.kt
+++ app/src/main/java/com/danieldisu/hnnotify/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.danieldisu.hnnotify.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.*
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
@@ -25,33 +26,51 @@ class MainActivity : AppCompatActivity() {
 
             MaterialTheme {
                 Scaffold(
-                    bottomBar = {
-                        BottomAppBar {
-                            IconButton(
-                                modifier = Modifier.weight(1f),
-                                onClick = mainViewModel::onHomeIconClick
-                            ) {
-                                Icon(
-                                    asset = vectorResource(id = R.drawable.ic_home)
-                                )
-                            }
-
-                            IconButton(
-                                modifier = Modifier.weight(1f),
-                                onClick = mainViewModel::onInterestsButtonClick
-                            ) {
-                                Icon(
-                                    asset = vectorResource(id = R.drawable.ic_book)
-                                )
-                            }
-                        }
-                    }
+                    bottomBar = bottomBar(),
+                    floatingActionButton = actionButton(state.value.selectedTab)
                 ) {
                     when (state.value.selectedTab) {
                         Tab.TopStories -> TopStoriesScreen(topStoriesViewModel)
                         Tab.Interests -> InterestsScreen()
                     }
                 }
+            }
+        }
+    }
+
+    private fun actionButton(selectedTab: Tab): @Composable() () -> Unit = {
+        when (selectedTab) {
+            Tab.TopStories -> {
+            }
+            Tab.Interests -> FloatingActionButton(
+                onClick = {},
+                icon = {
+                    Icon(
+                        asset = vectorResource(id = R.drawable.ic_add)
+                    )
+                }
+            )
+        }
+    }
+
+    private fun bottomBar(): @Composable() () -> Unit = {
+        BottomAppBar {
+            IconButton(
+                modifier = Modifier.weight(1f),
+                onClick = mainViewModel::onHomeIconClick
+            ) {
+                Icon(
+                    asset = vectorResource(id = R.drawable.ic_home)
+                )
+            }
+
+            IconButton(
+                modifier = Modifier.weight(1f),
+                onClick = mainViewModel::onInterestsButtonClick
+            ) {
+                Icon(
+                    asset = vectorResource(id = R.drawable.ic_book)
+                )
             }
         }
     }
